c1de2965568063e4757718cc1a19adc259c2691f
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-08 18:01:19 +0200

    update e2e test

--- end-to-end-test.sh
+++ end-to-end-test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-front=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3000/healthcheck)
-test "$front" = 200
 server=$(curl --silent --show-error http://localhost:8080/healthcheck)
 test "$server" = "OK"
 
