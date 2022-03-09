56d9622fda78f7effd471a00e8fb02f522266e8d
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-09 09:14:00 +0200

    second test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Todo from './components/Todo';
 
-test('renders learn react link', () => {
-  const name = 'Test Message'
-  render(<Todo name={name} completed={false} />)
-  expect(screen.getByText(name)).toBeInTheDocument();
+describe('todos', () => {
+  test('renders', () => {
+    const name = 'Test Message'
+    render(<Todo name={name} completed={false} />)
+    expect(screen.getByText(name)).toBeInTheDocument();
+  });
+
+  test('can be clicked', () => {
+    render(<Todo name="test" completed={false} />)
+    fireEvent.click(screen.getByText("test"));
+    expect(screen.getByText("test")).toHaveClass('completed');
+  });
 });
--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo({name, completed}) {
+  const [isCompleted, setCompleted] = useState(completed);
+
+  const toggle = () => {
+    setCompleted(!isCompleted);
+  } 
+
   return (
-    <div>
-      {name} {completed}
+    <div className={isCompleted ? 'completed' : ''} onClick={toggle}>
+      {name}
     </div>
   );
 }
