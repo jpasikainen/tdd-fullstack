48054d036b32c1facaab9c083c7cee4f70368031
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-09 08:41:54 +0200

    walking skeleton final phase

--- end-to-end-test.sh
+++ end-to-end-test.sh
@@ -4,4 +4,7 @@ set -euxo pipefail
 server=$(curl --silent --show-error http://localhost:8080/healthcheck)
 test "$server" = "OK"
 
+front=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3000)
+test "$front" = 200
+
 : OK
