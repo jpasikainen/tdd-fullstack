b365b98543860f12a0d030836d1786e532bda598
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-09 09:15:13 +0200

    testing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -8,9 +8,15 @@ describe('todos', () => {
     expect(screen.getByText(name)).toBeInTheDocument();
   });
 
-  test('can be clicked', () => {
+  test('can be marked as completed', () => {
     render(<Todo name="test" completed={false} />)
     fireEvent.click(screen.getByText("test"));
     expect(screen.getByText("test")).toHaveClass('completed');
+  });
+
+  test('can returned back to not completed', () => {
+    render(<Todo name="test" completed={true} />)
+    fireEvent.click(screen.getByText("test"));
+    expect(screen.getByText("test")).not.toHaveClass('completed');
   });
 });
