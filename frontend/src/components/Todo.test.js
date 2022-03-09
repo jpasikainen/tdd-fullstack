9cbfeebe6afe79ea1579c4b0cab67f341949c8a3
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-09 09:50:40 +0200

    organize

--- frontend/src/components/Todo.test.js
+++ frontend/src/components/Todo.test.js
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Todo from './components/Todo';
+import Todo from './Todo';
 
 describe('todos', () => {
   test('renders', () => {
