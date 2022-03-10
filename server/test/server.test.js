e5c6dd68b78a6bc547dc0b85804c1dd07762e61f
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-10 17:22:42 +0200

    test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,20 +1,31 @@
 var sinon = require('sinon');
 var chai = require('chai');
 var expect = chai.expect;
-var { create } = require("../todoController");
+var controller = require("../todoController.js");
 const flushPromises = () => new Promise(setImmediate);
 
 describe('server', () => {
+  let req, res;
+  beforeEach(() => {
+    req = { body: {} };
+    res = { code: sinon.stub().returnsThis(), send: sinon.stub() };
+  });
+
   afterEach(() => {
     sinon.restore();
   });
 
   it('adds data', async () => {
-    const req = { body: {} };
-    const res = { code: sinon.stub().returnsThis(), send: sinon.stub() };
-    create(req, res);
+    controller.create(req, res);
     await flushPromises();
     sinon.assert.calledWith(res.code, 201);
     sinon.assert.calledWith(res.send, {message: 'created'});
+  });
+
+  it('adds data', async () => {
+    controller.delete(req, res);
+    await flushPromises();
+    sinon.assert.calledWith(res.code, 204);
+    sinon.assert.calledWith(res.send, {message: 'deleted'});
   });
 });
