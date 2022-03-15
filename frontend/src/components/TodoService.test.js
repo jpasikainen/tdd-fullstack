9ea9c35f20d60711b4ac4c84b89632a46e6dfd23
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-15 19:41:31 +0200

    tdd style test first code second

--- frontend/src/components/TodoService.test.js
+++ frontend/src/components/TodoService.test.js
@@ -1,18 +1,28 @@
 import {getAll, create, put, deleteAll} from './TodoService'
 
-const fetchMock = jest
-  .spyOn(global, 'fetch')
-  .mockImplementation(() => Promise.resolve({ json: () => Promise.resolve([]) }))
+describe('service', () => {    
+  let fetchMock;
+  beforeEach(() => {
+    fetchMock = jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue({id: 1})
+    })
+  });
+  
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
-beforeEach(() => {
-  fetch.mockClear();
-});
-
-describe('service', () => {
   it('gets all', async () => {
     await getAll();
     expect(fetchMock).toHaveBeenCalledTimes(1);
   });
 
-
+  it('gets id back when creating', async () => {
+    const id = await create('foo');
+    expect(fetchMock).toHaveBeenCalledWith(
+      "http://localhost:8080/",
+      {"body": "{\"name\":\"foo\"}", "headers": {"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"}, "method": "POST", "mode": "cors"}
+    );
+    expect(id).toEqual({"id": 1});
+  })
 })
